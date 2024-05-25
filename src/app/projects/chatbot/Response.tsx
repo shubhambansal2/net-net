da3dcf1a2903d94f67ddae801b1fea7b2fc61a3b
Anurag_Sethia <42603789+anuragsethia@users.noreply.github.com>
2024-05-25 14:03:57 +0530

    New feature (#5)
    * Tranformed the bot to Mistral AI chatbot with Langchain
    
    * added decorator for ignore
    
    * added axios package in package.json
    
    ---------
    
    Co-authored-by: shubhambansal2 <shubhambansal1133@gmail.com>
    Co-authored-by: anurag-sethia <anurag.sethia@razorpay.com>

--- src/app/projects/chatbot/Response.tsx
+++ src/app/projects/chatbot/Response.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {Client, SpaceStatus} from "@gradio/client";
 import { Buffer } from 'buffer';
 import {Console} from "inspector"; // Ensure Buffer is imported from the polyfilled version
@@ -8,32 +9,15 @@ interface resp {
 
 export default async function Response(input: string) {
     try {
-        const client = await Client.connect("Shubhambansal2/mistralai-Mixtral-8x7B-Instruct-v0.1");
-        const result = await client.predict("/predict", { 		
-                        param_0: input,
-        });
-        // @ts-ignore
-        console.log(result.data);
-        // @ts-ignore
-        return result.data[0]
+      // Make a POST request to your Flask app on Heroku
+      const response = await axios.post('https://desolate-bastion-55476-3d3016c3fa1a.herokuapp.com/chat', {
+        text: input,
+      });
+      // Return the response from your Flask app
+      // @ts-ignore
+      return response.data.response;
     } catch (error) {
-        console.log(error);
-        throw error;
+      console.log(error);
+      throw error;
     }
-}
-
-   // const result = await app.predict("/predict", [
-        //     exampleImage, 	// blob in 'image' Image component
-        // ]);
-        // console.log("########");
-        // console.log(result);
-        // console.log(result.data[0].label);
-        // const app = await Client.connect("https://theblueberry-ai-mistral-7b-v0-2.hf.space/--replicas/ybv3y/", {space_status: (space_status: SpaceStatus) => console.log(space_status)});
-        // console.log(app);
-        // console.log("-------------");
-        // const response = await app.predict("/chat", [
-        //     input, 0 // number (numeric value between 0.0 and 1.0) in 'Temperature' Slider component
-        // ]);
-        // console.log(response);
-        // console.log("~~~~~~");
-        // @ts-ignore
+  }
