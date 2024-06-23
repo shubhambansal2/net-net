e503e5849ac844d459f8201e9574c41159ec920d
anurag-sethia <anurag.sethia@razorpay.com>
2024-06-23 17:55:42 +0530

    handling chatbot build

--- src/app/chatbot/layout.tsx
+++ src/app/chatbot/layout.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 
 const Layout = ({children,}: Readonly<{ children: React.ReactNode; }>) => {
     return (
         <div>
+            <Suspense fallback={<div>Loading...</div>}>
             {children}
+            </Suspense>
         </div>
     );
 };
