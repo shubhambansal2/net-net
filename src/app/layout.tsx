dea49779ad5bc7f1a7be14dc14451ccdbc2a08ae
Anurag_Sethia <42603789+anuragsethia@users.noreply.github.com>
2024-06-29 16:41:26 +0530

    adding gtag (#46)
    Co-authored-by: anurag-sethia <anurag.sethia@razorpay.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/header/Navbar";
+import Script from "next/script";
 
 
 const fontSans = Inter({
@@ -21,7 +22,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+    <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SDF5CRC3VJ"></Script>
+        <Script id="google-analytics">
+            {
+                `
+             window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SDF5CRC3VJ');
+           `
+            }
+        </Script>
+    </head>
       <body>
         {/*<div className={cn("relative w-full flex flex-col items-center justify-center", fontSans.variable) }>*/}
             <Navbar/>
