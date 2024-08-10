4878bbeea4efdc4e7f507634322bb5f793dc1b9e
shubhambansal2 <shubhambansal1133@gmail.com>
2024-08-10 21:30:01 +0530

    minor ui changes

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -7,7 +7,8 @@ import { motion } from "framer-motion";
 
 export default function Signup() {
   return (
-    <div className="w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+    // <div className="w-full h-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+    <div className="mt-40">
     {/*<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">*/}
     {/*</div>*/}
       <SignupFormDemo />
